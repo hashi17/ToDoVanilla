79b3ef895a2f7fc8f10f03d3b03600eb20269ac4
hashi17 <h.mabo3@gmail.com>
2021-08-04 18:32:47 +0900

    レイアウトとボタン表示実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得して初期化する
+  const inputText = document.getElementById("add-text").value;
+  console.log(inputText);
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className = "todo-txt";
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    console.log("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    console.log("削除");
+  });
+
+  // liタグの下にdivタグを入れる
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  console.log(li);
+  console.log(div);
+  console.log(p);
+  console.log(completeButton);
+  console.log(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
